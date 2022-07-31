86befb784a8461f2252a11e9364c642ead893cc8
Guy Whale <guywhale@gmail.com>
2022-07-31 14:40:14 +0100

    Add useImperativeHandle() to input component. This allows ref attributes set in the parent component to run internal component methods

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, {
+	useState,
+	useEffect,
+	useReducer,
+	useContext,
+	useRef,
+} from 'react';
 import AuthContext from '../store/auth-context';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -46,6 +52,9 @@ const Login = (props) => {
 
 	const authContext = useContext(AuthContext);
 
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+
 	useEffect(() => {
 		const identifier = setTimeout(() => {
 			console.log('Checking form validity!');
@@ -76,13 +85,20 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		authContext.onLogin(emailState.value, passwordState.value);
+		if (formIsValid) {
+			authContext.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
 	};
 
 	return (
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
 				<Input
+					ref={emailInputRef}
 					isValid={emailIsValid}
 					id="email"
 					label="E-mail"
@@ -93,6 +109,7 @@ const Login = (props) => {
 				/>
 
 				<Input
+					ref={passwordInputRef}
 					isValid={passwordIsValid}
 					id="password"
 					label="Password"
@@ -102,7 +119,7 @@ const Login = (props) => {
 					onBlue={validatePasswordHandler}
 				/>
 				<div className={classes.actions}>
-					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
+					<Button type="submit" className={classes.btn}>
 						Login
 					</Button>
 				</div>
